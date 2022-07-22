ce381a6448c6210c03aa07c90741a16b636b318a
Fcedilloo <53921491+Fcedilloo@users.noreply.github.com>
2022-07-21 21:54:49 -0500

    Create puzzle.php
    pagina para seleccionar tamaño del tablero

---
+++ puzzle.php
@@ -0,0 +1,42 @@
+<HTML>
+<HEAD>
+<TITLE>
+Puzzle Queens ajedrez
+</TITLE>
+<meta charset="ISO-8859-1">
+</head>
+<body>
+<center><h1>Puzzle N Quenns en un tablero de ajedrez</h1></center>
+<br>
+<br>
+<form name="puzzle" method="get" action="queens.php">
+<table align = "center">
+<tr>
+<td align = "center">
+Elige el tablero a resolver:
+</td>
+<td>
+<select name="tablero" id = "tablero">
+  <option value="8">8</option>
+  <option value="9">9</option>
+  <option value="10">10</option>
+  <option value="11">11</option>
+  <option value="12">12</option>
+  <option value="13">13</option>
+  <option value="14">14</option>
+  <option value="15">15</option>
+  <option value="16">16</option>
+  <option value="17">17</option>
+  <option value="18">18</option>
+  <option value="19">19</option>
+  <option value="20">20</option>
+</select>
+</td>
+<td>
+<span class="body">
+<input type="submit"/>
+</span>
+</td>
+</tr>
+</table>
+</form>
